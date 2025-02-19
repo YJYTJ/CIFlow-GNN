1d8267808ed4246c9d5ee5b83588373b75001303
YJYTJ <51269506+YJYTJ@users.noreply.github.com>
2025-02-19 21:16:12 +0800

    Add files via upload

--- classification/losses.py
+++ classification/losses.py
@@ -145,7 +145,7 @@ class Step_Loss(nn.Module):
                     mean2 = mean_list[j2]
                     center_loss_i += torch.mean((mean1 - mean2)**2)
             center_loss += -center_loss_i
-            center_loss = center_loss/(n_clusters*(n_clusters-1)/2)
+            center_loss = center_loss/(n_clusters-1)/2
         
         return feature_loss + center_loss 
     
