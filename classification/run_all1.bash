861a749fe132ba015f47d7c4925097286e3c339c
YJYTJ <51269506+YJYTJ@users.noreply.github.com>
2025-09-02 14:56:42 +0800

    Delete classification directory

--- classification/run_all1.bash
+++
@@ -1,9 +0,0 @@
-dataset=('MUTAG' 'BZR' 'BZR_MD' 'DHFR' 'COX2' 'PROTEINS' 'NCI1' 'DD')
-
-for A in "${dataset[@]}"
-do
- 
-    CUDA_VISIBLE_DEVICES=1 python main.py --dataset $A --use_node_labels
-        
-done
-
--- classification/run_all2.bash
+++
@@ -1,9 +0,0 @@
-dataset=('IMDB-BINARY' 'IMDB-MULTI')
-
-for A in "${dataset[@]}"
-do
- 
-    CUDA_VISIBLE_DEVICES=1 python main.py --dataset $A
-        
-done
-
