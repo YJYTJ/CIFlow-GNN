6501ec302ba843cd5af4ce1647c31bf66f4dc177
YJYTJ <51269506+YJYTJ@users.noreply.github.com>
2025-09-02 14:56:58 +0800

    Delete interpretation directory

--- interpretation/src/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mutag import Mutag
-from .motif import Motif
-from .solubility import SolubilityDataset
-from .benzene import BenzeneDataset
--- interpretation/src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .get_data_loaders import get_data_loaders
-from .get_model import  get_preds
-from .utils import *
-# from .scatter import *
-
